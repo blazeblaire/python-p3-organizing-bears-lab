a174b71dc005c54d1a0c3cf2b33d44c636ef37cb
TONNY <blare9.70@gmail.com>
2025-12-10 21:33:49 +0300

    ATP

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Aggressive', 1),
+('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+('Tabitha', 4, 'F', 'White', 'Calm', 1),
+('Sergeant Brown', 7, 'M', 'Brown', 'Gruff', 1),
+('Melissa', 2, 'F', 'Golden', 'Friendly', 1),
+('Grinch', 6, 'M', 'Green', 'Mean', 0),
+('Wendy', 3, 'F', 'Brown', 'Shy', 1),
+(NULL, 1, 'F', 'Black', 'Curious', 1);
